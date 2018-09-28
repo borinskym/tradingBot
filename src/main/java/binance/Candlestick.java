db052579f6a80c0328bed5e800dd09a59f8655c7
michael <borinskym@gmail.com>
2018-09-28 17:49:35 +0300

    first

---
+++ src/main/java/binance/Candlestick.java
@@ -0,0 +1,16 @@
+package binance;
+
+public class Candlestick {
+    long openTime;
+    double open;
+    double high;
+    double low;
+    double close;
+    double volume;
+    long closeTime;
+    double quoteAssetVolume;
+    long numberOfTrades;
+    double takerBuyBaseAssetVolume;
+    double takerBuyQuoteAssetVolume;
+    double ignore;
+}
---
+++ src/main/java/calculator/StochasticCalculator.java
@@ -0,0 +1,13 @@
+package calculator;
+
+import binance.Candlestick;
+
+import java.util.List;
+
+public class StochasticCalculator {
+
+    boolean calc(List<Candlestick> candlesticks){
+
+        return false;
+    }
+}
