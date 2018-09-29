85ff31a2e6443e2d42239ddaf374e72160a497f6
Romano <amitromano3@gmail.com>
2018-09-29 16:41:17 +0300

    Calculate stochastic (#1)

--- src/main/java/binance/Candlestick.java
+++ src/main/java/binance/Candlestick.java
@@ -1,5 +1,12 @@
 package binance;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+@Builder
 public class Candlestick {
     long openTime;
     double open;
--- src/main/java/calculator/StochasticCalculator.java
+++ src/main/java/calculator/StochasticCalculator.java
@@ -1,13 +1,95 @@
 package calculator;
 
 import binance.Candlestick;
+import com.sun.xml.internal.bind.v2.TODO;
 
 import java.util.List;
 
 public class StochasticCalculator {
+    enum Advice {BUY, SELL, STAY}
 
-    boolean calc(List<Candlestick> candlesticks){
+    int threshHold;
 
-        return false;
+    public StochasticCalculator(int threshHold) {
+        this.threshHold = threshHold;
+    }
+
+    Advice calc(List<Candlestick> candlesticks) {
+        if (isLessThenThreshHold(candlesticks))
+            return Advice.STAY;
+        else {
+            double result = getStochasticCalc(candlesticks);
+            if (isCross(result))
+                return Advice.STAY;
+            else if (isRedAboveBlue(result))
+                return Advice.SELL;
+            else
+                return Advice.BUY;
+        }
+    }
+
+    private boolean isLessThenThreshHold(List<Candlestick> candlesticks) {
+        return candlesticks.size() < threshHold;
+    }
+
+    private double getStochasticCalc(List<Candlestick> candlesticks) {
+        return getBlueValueForLastCandle(candlesticks) - getRedValueForLastCandle(candlesticks);
+    }
+
+    private double getBlueValueForLastCandle(List<Candlestick> candlesticks) {
+        return 100 *
+                (
+                        (getCurrentClose(candlesticks) - getLowestOfInterval(candlesticks)) /
+                                (getHighestOfInterval(candlesticks) - getLowestOfInterval(candlesticks))
+                );
+    }
+
+    private double getCurrentClose(List<Candlestick> candlesticks) {
+        return getCurrentCandle(candlesticks).getClose();
+    }
+
+    private double getHighestOfInterval(List<Candlestick> candlesticks) {
+        double result = getCurrentCandle(candlesticks).getLow();
+        for (int i = 1; i < 14; i++) {
+            double currLow = candlesticks.get(candlesticks.size() - 1 - i).getHigh();
+            if (currLow > result)
+                result = currLow;
+        }
+        return result;
+    }
+
+    private double getLowestOfInterval(List<Candlestick> candlesticks) {
+        double result = getCurrentCandle(candlesticks).getLow();
+        for (int i = 1; i < 14; i++) {
+            double currLow = candlesticks.get(candlesticks.size() - 1 - i).getLow();
+            if (currLow < result)
+                result = currLow;
+        }
+        return result;
+    }
+
+    private Candlestick getCurrentCandle(List<Candlestick> candlesticks) {
+        return candlesticks.get(candlesticks.size() - 1);
+    }
+
+
+    private double getRedValueForLastCandle(List<Candlestick> candlesticks) {
+        return average(
+                getBlueValueForLastCandle(candlesticks),
+                getBlueValueForLastCandle(candlesticks.subList(0, candlesticks.size() - 1)),
+                getBlueValueForLastCandle(candlesticks.subList(0, candlesticks.size() - 2))
+        );
+    }
+
+    private double average(double firstValue, double secondValue, double thirdValue) {
+        return (firstValue + secondValue + thirdValue / 3);
+    }
+
+    private boolean isRedAboveBlue(double result) {
+        return result < 0;
+    }
+
+    private boolean isCross(double result) {
+        return result == 0;
     }
 }
