dd88933b10f6c8e69a9a85710f0e8926a7280a6d
michael <borinskym@gmail.com>
2018-10-06 14:17:24 +0300

    continuing

---
+++ src/main/java/com/romanobori/binance/QueryStringBuilder.java
@@ -0,0 +1,20 @@
+package com.romanobori.binance;
+
+public class QueryStringBuilder {
+
+    String symbol;
+    String sideBuy;
+    String marketType;
+    String timeInForce;
+    int recvWindow;
+
+
+
+    String buy;
+
+
+    String buyString(double quantity){
+        return null;
+    }
+
+}
---
+++ src/test/java/com/romanobori/binance/QueryStringBuilderTest.java
@@ -0,0 +1,15 @@
+package com.romanobori.binance;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class QueryStringBuilderTest {
+
+
+    @Test
+    public void shouldReturnBuyString() {
+
+    }
+
+}
